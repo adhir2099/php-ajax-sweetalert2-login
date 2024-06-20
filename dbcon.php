4031811d2fc4fcff50248274fba1ae4c507711bd
Adhir DC <adhir.serrano@gmail.com>
2024-06-20 17:07:46 -0600

    Small change to connection, next update will be UI

--- dbcon.php
+++ dbcon.php
@@ -1,2 +1,10 @@
 <?php
-    $connect = new PDO("mysql:host=localhost;dbname=login;charset=utf8", "root", "");
+
+    try {
+        $connect = new PDO("mysql:host=host;dbname=dbname;charset=utf8", "user", "pass");
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        error_log("Database connection error: " . $e->getMessage());
+        echo "Oops! Something terribly wrong happened.";    
+        exit();
+    }
